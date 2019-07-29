ed84525f3e3d38f7090e6ef7796e315a5a68a766
James Osborne <jmosborne@6200D-127584-M.local>
2019-07-29 13:02:23 +1000

    fixing unused variables warnings

--- src/srn/VanLeeuwen2007NonDimSrnModel.cpp
+++ src/srn/VanLeeuwen2007NonDimSrnModel.cpp
@@ -88,8 +88,8 @@ void VanLeeuwen2007NonDimOdeSystem::EvaluateYDerivatives(double time, const std:
     double C_oT = rY[8]; // C_oT
     double C_cT = rY[9]; // C_cT
     double Y = rY[10]; // Y
-    double C_F = rY[11]; // C_F
-    double C_T = rY[12]; // C_T
+    double C_F;// = rY[11]; // C_F
+    double C_T;// = rY[12]; // C_T
     double drag = rY[13]; // drag
 
     /* Define state parameters */
